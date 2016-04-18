fa08e646059c1ec95f7e74e6e22067e0b6bd5d88
dartcharlie <buptcharlie@gmail.com>
2016-04-17 23:05:42 -0700

    tutorial 4 code

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,40 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404,render
 from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 from models import Question
 
 latest_question_display = 5
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:latest_question_display]
-	context = {
-		'latest_question_list':latest_question_list,
-	}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:latest_question_display]
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
-def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except(keyError,Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "Please select a valid choice",
+        })
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+
+    # redirect to results page
+	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
